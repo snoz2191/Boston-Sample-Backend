336a34e82557d1c7380fb1483f1376795aafc213
Miguel Fagundez <miguel_ef91@hotmail.com>
2015-05-02 08:36:01 -0430

    Testint Heroku Again

--- Tasks/models.py
+++ Tasks/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+# Login
+class Login(models.Model):
+  username = models.CharField(max_length=30)
+  password = models.CharField(max_length=16)
+
+class LoginSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Login
+
+# Status
+class Status(models.Model):
+  code = models.CharField(max_length=10)
+  msg = models.CharField(max_length=50)
+
+class StatusSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Status
+    fields = ('code','msg')
+# User
+class UserSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = User
+    fields = ('username','email','password')
--- Tasks/views.py
+++ Tasks/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
+from Tasks.models import Login, LoginSerializer, UserSerializer, Status, StatusSerializer
+from django.views.decorators.csrf import csrf_exempt
+
+class LoginService(APIView):
+
+    def post(self,request):
+      l = LoginSerializer(data=request.data)
+
+      # Validates format
+      if l.is_valid():
+        u = User.objects.get(username=l.validated_data["username"])
+      else:
+        status = Status(code="INVALID",msg="The format in the request is invalid")
+        return Response(StatusSerializer(status).data)
+
+      # Authenticates the user
+      user = authenticate(username=u.username, password=l.validated_data["password"])
+
+      if user is not None:
+        if user.is_active:
+          login(request, user)
+          status = Status(code="OK",msg="OK")
+          return Response(StatusSerializer(status).data)
+
+        else:
+          return Response("Error!")
+      else:
+        return Response("User is none")
+    def delete(self,request):
+      logout(request)
+      status = Status(code="OK",msg="OK")
+      return Response(StatusSerializer(status).data)
