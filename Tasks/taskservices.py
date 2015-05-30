9bd6f8af2fe6407319c242ff80d035f038f07eff
Domingo De Abreu <domingo.abreu@synergy-gb.com>
2015-05-30 18:55:54 -0430

    Added POST Service for Create a Task

--- Tasks/taskservices.py
+++ Tasks/taskservices.py
@@ -1,12 +1,36 @@
-
+from django.contrib.auth.models import User
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from Tasks.utils import STATUS_PARAMETERS_INVALID, STATUS_OK, STATUS_TASK_CREATOR_REQUIRED
 
-from .serializers import TaskSerializer
+from .serializers import TaskSerializer, StatusSerializer
 from .models import Task
 
 # List Tasks
 class TaskListService(APIView):
-  def get(self, request):
-    task_list = TaskSerializer(Task.objects.all(), many=True)
-    return Response(task_list.data)
+    def get(self, request):
+        task_list = TaskSerializer(Task.objects.all(), many=True)
+        return Response(task_list.data)
+
+    def post(self, request):
+        try:
+            task = TaskSerializer(data=request.data)
+        except:
+            status = STATUS_PARAMETERS_INVALID
+            return Response(StatusSerializer(status).data)
+
+        try:
+            task.initial_data["creator"] = User.objects.get(username=task.initial_data["creator"]).pk
+        except User.DoesNotExist:
+            status = STATUS_PARAMETERS_INVALID
+            return Response(StatusSerializer(status).data)
+        except KeyError:
+            status = STATUS_TASK_CREATOR_REQUIRED
+            return Response(StatusSerializer(status).data)
+
+        if task.is_valid():
+            task.save()
+            status = STATUS_OK
+            return Response(StatusSerializer(status).data)
+        else:
+            return Response(task.errors)
--- Tasks/utils.py
+++ Tasks/utils.py
@@ -7,3 +7,6 @@ STATUS_PARAMETERS_INVALID = Status(code="INVALID_PARAMETERS",msg="The format in 
 # For user
 STATUS_USER_INACTIVE = Status(code="INACTIVE", msg="User is inactive")
 STATUS_USER_INVALID = Status(code="INVALID_USER", msg="User is not registered")
+
+# For Task
+STATUS_TASK_CREATOR_REQUIRED = Status(code="REQUIRED_PARAMETERS", msg="The creator field is required")
